b0ac78fe2de4b28ec675e5c634b86062324a6f88
EduardoJosueAlvarez12 <97970428+EduardoJosueAlvarez12@users.noreply.github.com>
2022-02-10 12:42:24 -0600

    Proyectos Semana4

---
+++ Tarea/conversionessemana4/src/App.vue
@@ -0,0 +1,55 @@
+<template>
+  <div id="app">
+
+    <h1>Cálculo de unidades</h1>
+
+    <hr size="8" style="color:blue; background-color:blue">
+
+    <br>
+
+    <div class="row row-cols-1 row-cols-md-3 g-4">
+      <div class="col">
+        <time-conversion/>
+      </div>
+      <div class="col">
+        <time-conversion/>
+      </div>
+      <div class="col">
+        <time-conversion/>
+      </div>
+      <div class="col">
+        <time-conversion/>
+      </div>
+      <div class="col">
+        <time-conversion/>
+      </div>
+      <div class="col">
+        <time-conversion/>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import TimeConversion from './components/TimeConversion.vue'
+
+export default {
+  name: 'App',
+  components: {
+    TimeConversion
+  }
+}
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
---
+++ Tarea/conversionessemana4/src/components/TimeConversion.vue
@@ -0,0 +1,83 @@
+<template>
+    <div id="root">
+
+        <div class="card" style="width: 24rem;">
+            <div class="card-header text-white bg-dark mb-3">Convertir tiempo</div>
+            <div class="card-body">
+                <p class="card-text">
+                <table>
+                    <thead>
+                        <th>Opciones</th>
+                        <th>Valores</th>
+                    </thead>
+                    <tbody>
+                        <tr>
+                            <td>
+                                <select class="form-select" aria-label="Default select example" v-model="seleccion1">
+                                    <option value="As">Años</option>
+                                    <option value="Ms">Meses</option>
+                                    <option value="Sms">Semanas</option>
+                                    <option value="Ds">Días</option>
+                                    <option value='Hrs'>Horas</option>
+                                    <option value='Mins'>Minutos</option>
+                                    <option value='Secs'>Segundos</option>
+                                </select>
+                            </td>
+                            <td><input type="number" v-model="dato"></td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <select class="form-select" aria-label="Default select example" v-model="seleccion2">
+                                    <option value="As">Años</option>
+                                    <option value="Ms">Meses</option>
+                                    <option value="Sms">Semanas</option>
+                                    <option value="Ds">Días</option>
+                                    <option value='Hrs'>Horas</option>
+                                    <option value='Mins'>Minutos</option>
+                                    <option value='Secs'>Segundos</option>
+                                </select>
+                            </td>
+                            <td><input type="number" v-model="resultado" readonly></td>
+                        </tr>
+                    </tbody>
+                </table>
+                </p>
+                <button class="btn btn-dark" v-on:click="convertirT()">Calcular</button>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name:'TimeConversion',
+    data(){
+        return{
+            seleccion1:'Mins',
+            seleccion2:'Secs',
+            dato:0,
+            resultado:0
+        }
+    },
+    methods:{
+        convertirT(){
+            //horas
+           if (this.seleccion1 == 'Hrs' && this.seleccion2 == 'As') {
+                this.resultado = this.dato/8760;
+            } else if (this.seleccion1 == 'Hrs' && this.seleccion2 == 'Ms') {
+                this.resultado = this.dato/730;
+            } else if (this.seleccion1 == 'Hrs' && this.seleccion2 == 'Sms') {
+                this.resultado = this.dato/168;
+            } else if (this.seleccion1 == 'Hrs' && this.seleccion2 == 'Ds') {
+                this.resultado = this.dato/24;
+            } else if (this.seleccion1 == 'Hrs' && this.seleccion2 == 'Hrs') {
+                this.resultado = this.dato;
+            } else if (this.seleccion1 == 'Hrs' && this.seleccion2 == 'Mins') {
+                this.resultado = this.dato * 60;
+            } else if (this.seleccion1 == 'Hrs' && this.seleccion2 == 'Secs') {
+                this.resultado = this.dato * 3600;
+            }
+        }
+    }
+}
+</script>
