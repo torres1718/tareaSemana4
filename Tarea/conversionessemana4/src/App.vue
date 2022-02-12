a4ebc3a18b51c9f04e5868c10369258b32239907
EduardoJosueAlvarez12 <97970428+EduardoJosueAlvarez12@users.noreply.github.com>
2022-02-11 20:36:30 -0600

    Actualización de la tarea

--- Tarea/conversionessemana4/src/App.vue
+++ Tarea/conversionessemana4/src/App.vue
@@ -1,44 +1,49 @@
-<template>
-  <div id="app">
-
-    <h1>Cálculo de unidades</h1>
-
-    <hr size="8" style="color:blue; background-color:blue">
-
-    <br>
-
-    <div class="row row-cols-1 row-cols-md-3 g-4">
-      <div class="col">
-        <time-conversion/>
-      </div>
-      <div class="col">
-        <time-conversion/>
-      </div>
-      <div class="col">
-        <time-conversion/>
-      </div>
-      <div class="col">
-        <time-conversion/>
-      </div>
-      <div class="col">
-        <time-conversion/>
-      </div>
-      <div class="col">
-        <time-conversion/>
-      </div>
+<template >
+  <div id="app" :style="{'background-image':'url(https://images.pexels.com/photos/6863515/pexels-photo-6863515.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940)', 'background-size':'1200px 720px'}">
+    <div class="fixed-top p-3 mb-2 text-white" style="background-color:indigo; margin-bottom">
+      <h1>Cálculo de unidades</h1>
     </div>
+    <br>
+    <br>
+    <center>
+      <div class="row row-cols-1 row-cols-md-3 g-4">     <!-- div que contiene los componentes en columnas -->
+        
+        <div class="col">
+          <time-conversion/>
+        </div>
+        
+        <div class="col">
+          <mass-conversion/>
+        </div>
 
+        <div class="col">
+          <time-conversion/>
+        </div>
+
+        <div class="col">
+          <time-conversion/>
+        </div>
+
+        <div class="col">
+          <time-conversion/>
+        </div>
+        
+      </div>
+
+    </center>
   </div>
 </template>
 
 <script>
 // import HelloWorld from './components/HelloWorld.vue'
 import TimeConversion from './components/TimeConversion.vue'
+import MassConversion from './components/MassConversion.vue'
 
 export default {
   name: 'App',
   components: {
-    TimeConversion
+    TimeConversion,
+    MassConversion
   }
 }
 </script>
